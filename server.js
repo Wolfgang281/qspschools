ee84618b751b1028197612b08508a1d356e01fe0
Wolfgang281 <utkarshgupta.281@gmail.com>
2024-03-26 17:17:16 +0530

    added cors

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ let { DbConnection } = require("./config/db");
 let contentRoute = require("./routes/contentRoute");
 let chapterRoute = require("./routes/chapterRoute");
 let courseRoute = require("./routes/courseRoute");
+const cors = require("cors");
 
 let app = express();
 
 DbConnection();
 
 app.use(express.json());
+app.use(cors());
 app.use("/content", contentRoute);
 app.use("/chapter", chapterRoute);
 app.use("/course", courseRoute);
